b3dc65f4ff39d43bf34192871dcc650e68e6beea
Dan Jacob <dansamueljacob@gmail.com>
2017-07-18 18:37:38 -0700

    add validation to ensure amount of products can 't be less than 0 if users adds to cart

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,5 +2,6 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
   validates :quantity, :presence => true;
+  validates :quantity, :numericality => { :greater_than_or_equal_to => 0 }
 
 end
